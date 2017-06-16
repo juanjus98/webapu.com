7ff2360b63e3b9fcab1bf68f309b07558ebcbde5
Juan Julio Sandoval Layza <juanjus98@gmail.com>
2017-06-16 00:04:00 -0500

    Iniciando Menu

--- build/js/app.js
+++ build/js/app.js
@@ -1,43 +1,30 @@
 $(function() {
-	$('[data-toggle="tooltip"]').tooltip();
 
-	/**
-	 * Cargar Agentes
-	 */
-	var urlListar = base_url +  'phpjson/listarAgentes.php';
-	var dataListar = { queue : queueSelect};
-	$.getDataJson(urlListar,dataListar, $.setAgentes);
+	$('#menu-buton').click(function () {
+		console.log("Open!");
+		var $mainMenu = $('#main-menu');
+		var $animation = $mainMenu.data('animation');
+		
+	$($mainMenu).addClass('main-menu-open '+$animation+' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+		//$(this).removeClass('');
+		console.log('Abierto configurar animaciòn de menu');
+		//Aca mostrar animacion de menu.
+	});
 
-	/**
-	 * Actualizar estados de agentes
-	 */
-	setInterval( function() { $.getDataJson(urlListar,dataListar, $.setEstadosAgentes); }, 1000 );
+	});
 
-	/**
-	 * Cargar llamadas
-	 */
-	/*var urlLlamadas = base_url +  'phpjson/listarLlamadas.php';
-	var dataLlamadas = { queue : queueSelect, tipo : 'LIST'};
-	$.getDataJson(urlLlamadas,dataLlamadas, $.setLlamadas);*/
 
-	/**
-	 * Recargar llamadas
-	 */
-	/*dataLlamadas = { queue : queueSelect, tipo : 'RELIST'};
-	setInterval( function() { $.getDataJson(urlLlamadas,dataLlamadas, $.setNuevasLlamadas); }, 1000 );*/
+	/*$('#simple-menu').sidr({
+		timing: 'ease-in-out',
+		speed: 500
+	});
 
-	/**
-	 * Actualizar estados de llamadas (en-cola, activa)
-	 */
-	/*setInterval($.upEstadoLLamadas, 1000);*/
+	$( window ).resize(function () {
+	  $.sidr('close', 'sidr');
+	});
 
-	/**
-	 * Cargar llamadas totales
-	 */
-	var urlLlamadasTotales = base_url +  'phpjson/listarLlamadasTotales.php';
-	var dataLlamadasTotales = { queue : queueSelect };
-	setInterval( function() { $.getDataJson(urlLlamadasTotales,dataLlamadasTotales, $.setLlamadasTotales); }, 1000 );
-
-	/*sortTable();*/
+	$('#close-menu-button').click(function () {
+	  $.sidr('close', 'sidr');
+	});*/
 
 });
